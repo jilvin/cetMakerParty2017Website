48da1bd79c15a37e7936e92fe828d6c3a9f70ed3
Jilvin <jilvinjacob@gmail.com>
2017-05-07 13:09:00 +0530

    added .htaccess in html folder

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -8,10 +8,9 @@
   <meta name="description" content="Maker Party of College of Engineering Trivandrum, a joint venture by all the clubs of the college.">
   <meta name="author" content="Webmaster, CET Maker Party">
 
-  <link rel="stylesheet" href="<?php echo base_url();?>css/styles.css">
+  <link rel="stylesheet" href="<?php echo base_url();?>assets/css/style.css">
 
   <!--[if lt IE 9]>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
   <![endif]-->
 </head>
-<?php echo base_url();?>
