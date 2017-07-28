0d2c10359f1258b613b4f69ea256c43e8dcf4be9
Jilvin <jilvinjacob@gmail.com>
2017-07-29 04:36:09 +0530

    minor bug fix -- config

--- application/config/config.php
+++ application/config/config.php
@@ -23,17 +23,17 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-if($_SERVER['HTTP_HOST'] == 'localhost' || '127.0.0.1')
+if($_SERVER['HTTP_HOST'] == 'localhost' || $_SERVER['HTTP_HOST'] == '127.0.0.1')
 {
 
-$config['base_url'] = '';
+  $config['base_url'] = '';
 
 }
 else
 {
 
   $config['base_url'] = 'https://www.cetmakerparty.org/';
-  
+
 }
 
 /*
--- html/CONSTANTS.php
+++ html/CONSTANTS.php
@@ -1,6 +1,6 @@
 <?php
 
-if($_SERVER['HTTP_HOST'] == 'localhost' || '127.0.0.1')
+if($_SERVER['HTTP_HOST'] == 'localhost' || $_SERVER['HTTP_HOST'] == '127.0.0.1')
 {
   // For local testing. Remove after complete development.
   define('SITE_SERVER_BASE', 'http://'.$_SERVER['HTTP_HOST'].'/cetMakerParty2017Website/html/');
