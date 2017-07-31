b7c616d16669ca0c184488e83a8b3eb4dca6e355
Jilvin <jilvinjacob@gmail.com>
2017-07-31 16:08:12 +0530

    bug fix update in henosis + other changes in monalisa showcase

--- application/views/showcase/art/monalisa/showcase.php
+++ application/views/showcase/art/monalisa/showcase.php
@@ -2,6 +2,9 @@
 <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/css/sub/monalisa.css.php">
 
 <div id="showcaseMonalisa">
+  <h3 id="showcaseHeading">The most admired work in human history.</h3>
+  <h4 id="showcaseSubHeading">Start working on yours.</h4>
+  <a href="./user_authentication" class="btn btn-lg btn-default">Apply now</a>
 </div>
 
 <script src="<?php echo base_url();?>assets/js/sub/monalisa.js"></script>
