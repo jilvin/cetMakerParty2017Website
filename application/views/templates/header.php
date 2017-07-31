fbf0253fe9a17d2a034b3e485ef440e347f6325e
Jilvin <jilvinjacob@gmail.com>
2017-07-31 17:35:19 +0530

    added og meta tags

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -5,8 +5,12 @@
   <meta charset="utf-8">
 
   <title>CET Maker Party</title>
-  <meta name="description" content="Maker Party of College of Engineering Trivandrum, a joint venture by all the clubs of the college.">
+  <meta name="description" content="Official website of Maker Party - College of Engineering Trivandrum, a joint venture by all the clubs of the college.">
   <meta name="author" content="Webmaster, CET Maker Party">
+
+  <meta property="og:title" content="CET Maker Party" />
+  <meta property="og:description" content="Official website of Maker Party - College of Engineering Trivandrum, a joint venture by all the clubs of the college." />
+  <meta property="og:image" content="<?php echo base_url();?>favicon.png" />
 
   <!--[if lt IE 9]>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
@@ -14,7 +18,7 @@
 
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+  <link rel="shortcut icon" href="<?php echo base_url();?>favicon.ico" type="image/x-icon">
 
   <!-- Latest compiled and minified CSS - Bootstrap -->
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
