9c4ebebf8c2d521a812da7d30719da1534097226
Jilvin <jilvinjacob@gmail.com>
2017-08-12 22:00:36 +0530

    working on login

--- application/config/config.php
+++ application/config/config.php
@@ -46,7 +46,7 @@ else
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
