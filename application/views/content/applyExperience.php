f3a4bc0582cf0041eb320c02ef45af9624f55095
Jilvin <jilvinjacob@gmail.com>
2017-08-20 21:28:43 +0530

    saving changes

--- application/views/content/applyExperience.php
+++ application/views/content/applyExperience.php
@@ -1,14 +1,38 @@
+<?php
+  if(empty($artName))
+  {
+    $artName="";
+  }
+  if(empty($artShortDescription))
+  {
+    $artShortDescription="";
+  }
+  if(empty($artLongDescription))
+  {
+    $artLongDescription="";
+  }
+  if(empty($patronClub))
+  {
+    $patronClub="";
+  }
+ ?>
 <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/css/sub/apply.css.php">
 <div class="row">
   <div class="col-md-4">
     <div class="form_main">
       <h3 class="heading">Apply for your experience.<span></span></h3>
       <div class="form">
-        <form action="<?php echo base_url() ?>apply/complete" method="post" id="applicationForm" name="applicationForm">
-          <input id="inputWorkName" type="text" required="true" placeholder="Suggested name." value="" name="experienceName">
-          <input id="inputWorkName" type="text" required="true" placeholder="Short description." value="" name="experienceShortDescription">
-          <textarea id="textareaWorkDescription" required="true" placeholder="Long description." name="experienceLongDescription" type="text"></textarea>
-          <input type="submit" value="Apply" name="Apply">
+        <form action="<?php echo base_url() ?>apply/complete" method="post" id="applicationForm" name="applicationForm" enctype="multipart/form-data" accept-charset="utf-8">
+          <input id="inputWorkName" type="text" required="true" placeholder="Suggested name." value="<?php echo $artName; ?>" name="experienceName">
+          <input id="inputWorkName" type="text" required="true" placeholder="Short description." value="<?php echo $artShortDescription; ?>" name="experienceShortDescription">
+          <textarea id="textareaWorkDescription" required="true" placeholder="Long description." name="experienceLongDescription" type="text"><?php echo $artLongDescription; ?></textarea>
+          <p style="margin-top: 20px;">Upload an image with ratio: 38:35. Upload high quality image if possible.</p>
+          <span style="color:red;"><?php echo $error1;?></span>
+          <input type="file" name="userfile1" required="true" size="20" />
+          <p style="margin-top: 30px;">Upload an image with ratio: 16:3. Upload high quality image if possible.</p>
+          <span style="color:red;"><?php echo $error2;?></span>
+          <input type="file" name="userfile2" required="true" size="20" />
+          <input type="submit" value="Apply" name="Apply" style="margin-top: 30px;">
         </form>
       </div>
     </div>
