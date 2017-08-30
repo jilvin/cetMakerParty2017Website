a7131c07e803b80793f25948c879c3d725974b2c
Jilvin <jilvinjacob@gmail.com>
2017-08-30 16:24:16 +0530

    saving changes --safe state

--- application/views/administration/adminButtonsSection.php
+++ application/views/administration/adminButtonsSection.php
@@ -1,12 +1,18 @@
-<div id="adminButtonsSection" style="margin-top: 20px; margin-left: 15px;">
-  <span style="display: inline-block; padding-bottom: 10px;"><b>Admin Action Panel</b></span>
-<form action="<?php echo base_url(); ?>admin/approve" method="POST">
-    <input type="hidden" name="artID" value="<?php echo $artID; ?>">
-      <input type="submit" value="Approve" style="width: 125px; margin-top: 2px;">
-  </form>
-  <span style="display:inline-block; width: 15px;"></span>
-  <form action="<?php echo base_url(); ?>admin/reject" method="POST">
-    <input type="hidden" name="artID" value="<?php echo $artID; ?>">
-      <input type="submit" value="Reject" style="width: 125px; margin-top: -10px;">
-  </form>
+<div class="container">
+  <div class="row">
+    <div class="col-md-12">
+      <div id="adminButtonsSection" style="margin-top: 20px;">
+        <span style="display: inline-block; padding-bottom: 10px;"><b>Admin Action Panel</b></span>
+        <form action="<?php echo base_url(); ?>admin/approve" method="POST">
+          <input type="hidden" name="artID" value="<?php echo $artID; ?>">
+          <input type="submit" value="Approve" style="width: 125px; margin-top: 2px;">
+        </form>
+        <span style="display:inline-block; width: 15px;"></span>
+        <form action="<?php echo base_url(); ?>admin/reject" method="POST">
+          <input type="hidden" name="artID" value="<?php echo $artID; ?>">
+          <input type="submit" value="Reject" style="width: 125px; margin-top: -10px;">
+        </form>
+      </div>
+    </div>
+  </div>
 </div>
