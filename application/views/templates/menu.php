8f73819a89a5329f0c6339da2f4a1d80e2df7de0
Jilvin <jilvinjacob@gmail.com>
2017-10-30 20:03:09 +0530

    static addition of schedule for party 1 -- unsafe

--- application/views/templates/menu.php
+++ application/views/templates/menu.php
@@ -19,6 +19,7 @@
         <li><a href="<?php echo base_url();?>about" class="resumeHenosis">About</a></li>
         <li><a href="<?php echo base_url();?>works" class="resumeHenosis">Works</a></li>
         <li><a href="<?php echo base_url();?>experiences" class="resumeHenosis">Experiences</a></li>
+        <li><a href="<?php echo base_url();?>downloads/1/schedule.pdf" class="resumeHenosis">Schedule</a></li>
         <!-- <li><a href="<?php echo base_url();?>past" class="resumeHenosis">Past</a></li> -->
         <!-- <li><a href="<?php echo base_url();?>team" class="resumeHenosis">Team</a></li> -->
         <span id="menuUserSection">
